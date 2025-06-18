90cb61f4c4c8112118e112ef0213edcb34264db7
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-06-18 15:58:11 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable max-len */
+
 import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { MovieList } from './components/MovieList/MovieList';
@@ -9,10 +11,6 @@ export const App = () => (
     </div>
 
     <div className="sidebar" data-cy="Sidebar">
-      Sidebar will be here
-    </div>
-  </div>
-);
       Sidebar will be here
     </div>
   </div>
