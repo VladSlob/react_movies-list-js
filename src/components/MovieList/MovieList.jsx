c78a003128da3f3f9b3bf31209ab8b7ce34a68fe
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-06-18 15:57:31 +0300

    MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,8 +1,8 @@
-import './MovieList.scss';
 import { MovieCard } from '../MovieCard/MovieCard';
+import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
-  <div className="movies" data-cy="MovieList">
+  <div className="movies">
     {movies.map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
     ))}
